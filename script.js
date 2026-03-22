f1d2b81d5ab3ba285cbd537749fb17b9789b7c11
Yousef1011 <karsayousef74@gmail.com>
2026-03-22 08:22:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const gessNumUser = document.querySelector("#guessInput")
+const CheckBtn = document.querySelector("#checkBtn")
+const outBut = document.querySelector("#message")
+const loader = document.querySelector("#loader")
+
+CheckBtn.addEventListener("click", function () {
+
+  const UserValue = Number(gessNumUser.value)
+
+  if (!UserValue || isNaN(UserValue)) {
+    outBut.textContent = "Please enter a valid number"
+    return;
+  }
+
+  const guessNum = Math.trunc(Math.random() * 20) + 1
+  gessNumUser.value = ""
+  loader.style.display = "block";
+  outBut.textContent = ""
+
+  setTimeout(function () {
+    loader.style.display = "none"
+
+    if (UserValue === guessNum) {
+      outBut.textContent = "Nice!"
+    } else {
+      outBut.textContent = `You chose ${UserValue}, and the correct number is ${guessNum}.`
+    }
+
+  }, 1000)
+
+})
